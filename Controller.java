104e95710d949ab7587f09119c1d6593eedf6543
blakecalvin <blakecalvin@gmail.com>
2017-10-27 00:58:40 -0700

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,74 @@
+package calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField text;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button zero;
+
+    @FXML
+    private Button point;
+
+    @FXML
+    private Button leftPar;
+
+    @FXML
+    private Button rightPar;
+
+    @FXML
+    private Button plus;
+
+    @FXML
+    private Button minus;
+
+    @FXML
+    private Button multi;
+
+    @FXML
+    private Button div;
+
+    @FXML
+    private Button equal;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button clear;
+
+    public void one_click(){
+        System.out.print("one");
+    }
+
+}
+
